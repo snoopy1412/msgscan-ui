2d48a523f7cd91cdf7405b43fca9ed21f8f8ed03
ex_suzhonghui <ex_suzhonghui@tcl.com>
2024-06-03 22:22:05 +0800

    feat: Update AppQueryClientProvider to use lazy initialization for query client
    - Initialize query client lazily using React.useState hook
    - Set default options for queries with a stale time of 60 seconds

--- src/provider/AppQueryClientProvider.tsx
+++ src/provider/AppQueryClientProvider.tsx
@@ -3,11 +3,19 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
-
 const AppQueryClientProvider: React.FC<React.PropsWithChildren<unknown>> = ({
   children,
 }) => {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
