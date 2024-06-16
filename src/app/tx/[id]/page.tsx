715449d66ecbc0f411cf6a3767cb0449402c468f
ex_suzhonghui <ex_suzhonghui@tcl.com>
2024-06-16 11:50:41 +0800

    Prefetching data

--- src/app/tx/[id]/page.tsx
+++ src/app/tx/[id]/page.tsx
@@ -1,12 +1,22 @@
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import ClientPage from './components/ClientPage';
-import { NextPage } from 'next';
+import { fetchMessage } from '@/graphql/services';
 
 interface PageProps {
   params: {
     id: string;
   };
 }
-const Page: NextPage<PageProps> = ({ params }) => {
-  return <ClientPage id={params?.id} />;
-};
-export default Page;
+export default async function Page({ params }: PageProps) {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['message', params.id],
+    queryFn: async () => fetchMessage(params.id)
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ClientPage id={params?.id} />
+    </HydrationBoundary>
+  );
+}
