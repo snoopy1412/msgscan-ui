bb65f29b102a4bb085b13e96f2304dd501b1dbda
ex_suzhonghui <ex_suzhonghui@tcl.com>
2024-06-17 08:27:11 +0800

    refactor: Update API base URL configuration for deployment mode

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,8 +1,9 @@
-import { DEPLOY_ENV } from "@/types/env";
+import { DEPLOY_ENV } from '@/types/env';
 
 export const APP_BASE_API_URL_MAP = {
-  [DEPLOY_ENV.MAINNET]: "http://g2.generic.darwinia.network:42049",
-  [DEPLOY_ENV.TESTNET]: "http://g2.generic.darwinia.network:42049",
+  [DEPLOY_ENV.MAINNET]: 'http://g2.generic.darwinia.network:42049',
+  [DEPLOY_ENV.TESTNET]: 'http://g2.generic.darwinia.network:42049'
 };
 
-export const APP_BASE_API_URL = APP_BASE_API_URL_MAP[DEPLOY_ENV.MAINNET];
+export const APP_BASE_API_URL =
+  APP_BASE_API_URL_MAP[process.env.NEXT_PUBLIC_DEPLOYMENT_MODE as DEPLOY_ENV];
