86298c25cbed0cade920e2fcda84188d3e219ede
ex_suzhonghui <ex_suzhonghui@tcl.com>
2024-06-28 13:59:51 +0800

    refactor: Update app name and description in site configuration

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,5 @@
-export const APP_NAME = 'MsgPort';
-export const APP_DESCRIPTION = 'MsgPort scan';
+export const APP_NAME = 'Msgport scan';
+export const APP_DESCRIPTION = 'Msgport scan';
 
 //interface 5000 ms
 export const REFRESH_INTERVAL = 5000;
